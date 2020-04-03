12a7426754a4551c4dc24a8902e5113d848aefe5
tetsu5555 <tetete1118@gmail.com>
2020-04-03 12:07:05 +0900

    feat: add some test

---
+++ src/server.test.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const app = require("./app")
+
+describe("Test hello path", () => {
+  test("It should response the GET method", done => {
+    request(app)
+      .get("/hello")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe("Test the templates path", () => {
+  test("It should response the GET method", done => {
+    request(app)
+      .get("/api/templates")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  test("It should response the POST method without request body", done => {
+    request(app)
+      .post("/api/templates")
+      .then(response => {
+        expect(response.statusCode).toBe(500);
+        done();
+      });
+  });
+});
