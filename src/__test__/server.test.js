fcfd8f6d562fd49f04f8f7cf647a7b09cf5cf54d
tetsu5555 <tetete1118@gmail.com>
2020-04-03 12:29:34 +0900

    chore: move test under __test__ folder

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("./app")
+const app = require("../app")
 
 describe("Test hello path", () => {
   test("It should response the GET method", done => {
