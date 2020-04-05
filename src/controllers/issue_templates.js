681b7455ebde94e74bc44a976d0075f26ef32cd4
tetsu5555 <tetete1118@gmail.com>
2020-04-05 18:38:14 +0900

    feat; add IssueTemplateController

---
+++ src/controllers/issue_templates.js
@@ -0,0 +1,44 @@
+class IssueTemplateController {
+  constructor(issueTemplateService) {
+    this.issueTemplateService = issueTemplateService
+  }
+
+  async list(req, res, next) {
+    try {
+      const result = await this.issueTemplateService.getIssueTemplates()
+      res.json(result);
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  }
+
+  async show(req, res, next) {
+    try {
+      const result = await this.issueTemplateService.getIssueTemplateById(req.params.template_id, next)  
+      res.json(result);
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const result = await this.issueTemplateService.createIssueTemplate(req.body)
+      res.json(result);
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      const result = await this.issueTemplateService.deleteIssueTemplateById(req.params.template_id)
+      res.json(result);  
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = IssueTemplateController;
